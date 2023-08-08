d66cf732d0e67ddc51642a150185f3492b7ca499
ZekiDev <47107701+Zequy40@users.noreply.github.com>
2023-08-08 10:08:36 +0200

    header modified

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -20,9 +20,9 @@ function Header() {
       <div className='max-w-[760px] bg-gray-200 fixed w-full top-0 z-10'>
         <div className='w-full pb-4 pt-4 bg-gray-200 px-3'>
           <div className='flex justify-around mb-1 py-[2px]  bg-slate-500 rounded'>
-            <Link href="./../category"><button className={router === '/category' || router === '/category/Pollos' || router === '/category/Picar' || router === '/category/Tortillas' || router === '/category/Menu' || router === '/category/Bolsa' ? 'p-2 capitalize bg-slate-200 rounded' : 'p-2 capitalize text-white'}>categoria</button></Link>
-            <Link href="/"><button className={router === '/' ? 'p-2 capitalize bg-slate-200 rounded' : 'p-2 capitalize text-white'}>productos</button></Link>
-            <button className='p-2 capitalize text-white' onClick={handleCartToggle}>compras</button>
+            <Link href="./../category"><button className={router === '/category' || router === '/category/Pollos' || router === '/category/Picar' || router === '/category/Tortillas' || router === '/category/Menu' || router === '/category/Bolsa' ? 'p-2 capitalize bg-slate-200 rounded ' : 'p-2 capitalize text-white hover:bg-slate-200 hover:rounded hover:text-black'}>categoria</button></Link>
+            <Link href="/"><button className={router === '/' ? 'p-2 capitalize bg-slate-200 rounded ' : 'p-2 capitalize text-white hover:bg-slate-200 hover:rounded hover:text-black'}>productos</button></Link>
+            <button className='p-2 capitalize text-white hover:bg-slate-200 hover:rounded hover:text-black' onClick={handleCartToggle}>compras</button>
           </div>
         </div>
         
