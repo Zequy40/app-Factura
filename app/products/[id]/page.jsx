fb1ec48d5770c862730dc9c146ff8a1daf8e6cae
ZekiDev <47107701+Zequy40@users.noreply.github.com>
2023-10-04 10:07:15 +0200

    description bug fix css

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,15 +1,16 @@
 import { products } from '@/app/mocks/product.json';
 import Link from 'next/link';
+import ProductMob from './../../styles/ProductOnly.module.css'
 
 
 const Page = ({ params }) => {
   const categoryColors = {
-    Tortillas: 'bg-blue-500 text-white',
-    Menu: 'bg-orange-400 text-white',
-    Picar: 'bg-red-500 text-white',
-    Pollos: ' bg-yellow-700 text-white',
-    Pescado: ' bg-indigo-900 text-white',
-    Bolsa: 'bg-white text-black'
+    Tortillas: `${ProductMob.tortillaCat}`,
+  Menu: `${ProductMob.menuCat}`,
+  Picar: `${ProductMob.picarCat}`,
+  Pollos: `${ProductMob.polloCat}`,
+  Pescado: `${ProductMob.pescadoCat}`,
+  Bolsa: `${ProductMob.bolsaCat}`
   };
   const { id } = params;
   const idAsNumber = parseInt(id, 10);
@@ -26,22 +27,22 @@ const Page = ({ params }) => {
 
   return (
     <>
-      <div className="max-w-[760px] w-full h-[100vh] bg-gray-700 flex items-center justify-center">
-        <div className="flex flex-col gap-2 p-16 rounded-xl bg-gray-300 relative" >
-        <Link href={'/'} className="flex items-center justify-center border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 absolute w-12 h-12 top-1 left-1 rounded-full">X</Link>
-          <div className={`flex items-center justify-center p-4 rounded-lg text-center font-semibold uppercase ${categoryColors[product.category]}`}>{product.category}</div>
-          <div className="flex gap-4">
-            <div className="w-full text-lg p-1">{product.title}:</div>
-            <div className="flex justify-center items-center text-white">
-              <div className="card-text text-center text-black">{product.price}€</div>
+      <div className={ProductMob.header}>
+        <div className={ProductMob.contain} >
+        <Link href={'/'} className={ProductMob.btn}>X</Link>
+          <div className={`${ProductMob.cat} ${categoryColors[product.category]}`}>{product.category}</div>
+          <div className={ProductMob.card}>
+            <div className={ProductMob.title}>{product.title}:</div>
+            <div className={ProductMob.band}>
+              <div className={ProductMob.price}>{product.price}€</div>
               
 
             </div>
             
           </div>
-          <div className="flex flex-col mb-8">
-          <div className="text-xs">Descripción:</div>
-          <div className="text-md">{product.description}</div>
+          <div className={ProductMob.p}>
+          <div className={ProductMob.description}>Descripción:</div>
+          <div className={ProductMob.subDescription}>{product.description}</div>
           </div>
         
         </div>
